581404c4d8b5826947207abd8325ea0e974f0461
resslr <122219240+resslr@users.noreply.github.com>
2026-06-01 12:05:47 -0400

    fix: outdated tailwind logic

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
